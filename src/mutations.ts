7a4746f2f13310981ed5f8c3c235fc13dc74b5ce
Martin Middel <martin@elliat.nl>
2025-07-11 10:04:41 +0200

    feat(exist-db): add exist-db mutations for update node syntax

--- src/mutations.ts
+++ src/mutations.ts
@@ -7,10 +7,25 @@ export type Mutation = {
 const existDBMutations: Mutation[] = [
 	{
 		where: 'Expr',
-		name: 'ExistDB_UpdateExpr',
+		name: 'ExistDB_UpdateInsertExpr',
 		additionalRules: [
-			`ExistDB_UpdateExpr ::= 'update' 'insert' Expr ( 'into' | 'following' | 'preceding' ) ExprSingle`,
+			`ExistDB_UpdateInsertExpr ::= 'update' 'insert' Expr ( 'into' | 'following' | 'preceding' ) ExprSingle`,
 		],
+	},
+	{
+		where: 'Expr',
+		name: 'ExistDB_UpdateReplaceExpr',
+		additionalRules: [`ExistDB_UpdateReplaceExpr ::= 'update' 'replace' Expr with ExprSingle`],
+	},
+	{
+		where: 'Expr',
+		name: 'ExistDB_UpdateDeleteExpr',
+		additionalRules: [`ExistDB_UpdateDeleteExpr ::= 'update' 'delete' ExprSingle`],
+	},
+	{
+		where: 'Expr',
+		name: 'ExistDB_UpdateRenameExpr',
+		additionalRules: [`ExistDB_UpdateRenameExpr ::= 'update' 'rename' Expr 'as' ExprSingle`],
 	},
 ];
 
