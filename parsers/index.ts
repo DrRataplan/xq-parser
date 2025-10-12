e3ae3ce028e67a052d3a5dd8d0ed6611088c82b4
Martin Middel <martin@elliat.nl>
2025-10-12 14:31:29 +0200

    feat(types): export Terminal/NonTerminal types

--- parsers/index.ts
+++ parsers/index.ts
@@ -1,7 +1,7 @@
 import makeWrapper, { type WrappedParser } from './wrapper.ts';
 import * as XQuery4FullParser from './XQuery-40-full.ts';
 import * as XQuery31FullParser from './XQuery-31-full.ts';
-export { type Node } from '../shared/Node.ts';
+export { type Node, type Terminal, type NonTerminal } from '../shared/Node.ts';
 
 export const XQuery4Full = makeWrapper(XQuery4FullParser.Parser, 'parse_Module', XQuery4FullParser.ParseException);
 export const XQuery31Full = makeWrapper(XQuery31FullParser.Parser, 'parse_XQuery', XQuery31FullParser.ParseException);
